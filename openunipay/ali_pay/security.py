e21f925c70fc1f3a55682083747b21fd3f342457
eric.pan <pan.weifeng@live.cn>
2016-01-30 12:31:43 +0800

    fix alipay key file error

--- openunipay/ali_pay/security.py
+++ openunipay/ali_pay/security.py
@@ -1,6 +1,7 @@
 import os
 import rsa
 import base64
+from django.conf import settings
 from urllib.parse import quote_plus
 
 def sign(data):
@@ -21,13 +22,13 @@ def verify(valueDict):
     return result 
 
 def _load_private_key():
-    keyfilePath = os.path.join(os.path.dirname(__file__), 'key', 'rsa_private_key.pem')
+    keyfilePath = os.path.join(settings.ALIPAY['rsa_private_key_pem'])
     with open(keyfilePath) as keyFile:
         return rsa.PrivateKey.load_pkcs1(keyFile.read().encode('latin_1'))
     
     
 def _load_ali_pub_key():
-    keyfilePath = os.path.join(os.path.dirname(__file__), 'key', 'ali_rsa_pub_key.pem')
+    keyfilePath = os.path.join(settings.ALIPAY['ali_public_key_pem'])
     with open(keyfilePath) as keyFile:
         return rsa.PublicKey.load_pkcs1_openssl_pem(keyFile.read().encode('latin_1'))
 
